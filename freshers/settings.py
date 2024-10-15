f1a7764e0f35736608ecd4df2e45744e3bc8fbf2
Bene-volent <benevolent733@gmail.com>
2024-10-16 00:36:26 +0530

    Corsremoved

--- freshers/settings.py
+++ freshers/settings.py
@@ -39,24 +39,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
-CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOW_METHODS = (
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-)
-CORS_ALLOW_HEADERS = (
-    "accept",
-    "authorization",
-    "content-type",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-)
 # Application definition
 
 INSTALLED_APPS = [
@@ -69,14 +51,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'freshersAPI',
     'frontend',
-    'corsheaders',
 ]
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
