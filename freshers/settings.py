58c7b186b08eb0aa1a858a8eddee1cfde8a5944f
Bene-volent <benevolent733@gmail.com>
2024-10-16 01:40:50 +0530

    Kind of completed

--- freshers/settings.py
+++ freshers/settings.py
@@ -25,12 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 
-# STATIC_URL = '/static/'  # Base URL to access static files
-STATIC_URL = '/static/'  # Base URL to access static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  # Directory where your static files (CSS, JS) are located
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-cx$x(m7g6!)#=x_%bih#q0&p_@1w2j0-9&)k#c5a9=ddwha(a@'
 
